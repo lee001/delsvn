7988e3758cc480682f405e1763c214ef5e036425
zw <lzwdev@outlook.com>
2017-11-15 11:27:12 +0800

    @Title: 工厂模式之工厂方法模式(factory method) @CodeReview: zw

---
+++ src/com/example/factoryMethod/Driver.java
@@ -0,0 +1,12 @@
+package com.example.factoryMethod;
+
+import com.example.simpleFactory.Car;
+
+/**
+ * 工厂方法模式
+ * Created by zw on 2017/11/15.
+ * @author zw
+ */
+public interface Driver {
+    Car driveCar();
+}
---
+++ src/com/example/factoryMethod/QQDriver.java
@@ -0,0 +1,22 @@
+package com.example.factoryMethod;
+
+import com.example.simpleFactory.Car;
+import com.example.simpleFactory.QQCar;
+
+/**
+ * 工厂方法模式
+ * Created by zw on 2017/11/15.
+ * @author zw
+ */
+public class QQDriver implements Driver{
+    @Override
+    public Car driveCar() {
+        return new QQCar();
+    }
+
+    public static void main(String[] args) {
+        Driver driver = new QQDriver();
+        Car car = driver.driveCar();
+        car.drive();
+    }
+}
