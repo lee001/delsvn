524473c1b2269714ef8f175ca56f695b95fe7aa1
zw <lzwdev@outlook.com>
2017-11-15 12:04:31 +0800

    @Title: 原型模式(prototype) @CodeReview: zw

---
+++ src/com/example/Prototype/AbstractObject.java
@@ -0,0 +1,29 @@
+package com.example.Prototype;
+
+/**
+ * 原型模式(prototype)
+ * Created by zw on 2017/11/15.
+ * @author zw
+ */
+public abstract class AbstractObject implements Cloneable {
+
+    String objectName;
+
+    public void setObjectName(String spoonName){
+        this.objectName = objectName;
+    }
+
+    public String getObjectName(){
+        return this.objectName;
+    }
+
+    public Object clone(){
+        Object object = null;
+        try {
+            object = super.clone();
+        }catch (CloneNotSupportedException exception){
+            System.err.println("AbstractObject is not Cloneable");
+        }
+        return object;
+    }
+}
